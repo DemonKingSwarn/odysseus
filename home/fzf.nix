08a217cd12b4cb622ae9807ddfbbf3bdae52301e
DemonKingSwarn <rockingswarn@gmail.com>
2026-04-20 22:14:41 +0530

    ID:10T Error
     an automated commit

--- home/fzf.nix
+++ home/fzf.nix
@@ -1,9 +1,25 @@
 { lib, ... }:
 
 {
+
+
   programs.fzf = {
     enable = true;
-    colors = lib.mkForce { };
+    colors = lib.mkForce {
+      bg       = "#1e1e2e";
+      "bg+"    = "#313244";
+      fg       = "#cdd6f4";
+      "fg+"    = "#cdd6f4";
+      hl       = "#f38ba8";
+      "hl+"    = "#f38ba8";
+      header   = "#f38ba8";
+      info     = "#cba6f7";
+      marker   = "#b4befe";
+      pointer  = "#f5e0dc";
+      prompt   = "#cba6f7";
+      spinner  = "#f5e0dc";
+      selected-bg = "#45475a";
+    };
 
     defaultOptions = [
       "--height 40%"
